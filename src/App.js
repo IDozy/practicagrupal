b09c3a88089bf1dc7be005c9965b6bebcc121df2
Dozy <ahuatayc18_2@unc.edu.pe>
2025-02-04 09:23:49 -0500

    Ejemplo Componente

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import AlexComponent from "./components/AlexComponent";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
         Ahí mi gatito miau miau
         <h1>Dios es la piedad, Xd</h1>
       </div>
+
+      <AlexComponent />
     </div>
   );
 }
