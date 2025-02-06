e76bce094d1322c5bd3c85c45bbf4f05e3afc0b2
fernanCG15 <celisgelaciofernandomanuel>
2025-02-06 09:14:42 -0500

    Cambios importantes :D

--- src/components/Imagenescomponent.jsx
+++ src/components/Imagenescomponent.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Imagenescomponent = () => {
   return (
-    <div>
+    <div className='container' style={{backgroundcolor:"red"}}> 
       <div>
         <img src="https://static.vecteezy.com/system/resources/thumbnails/009/273/280/small/concept-of-loneliness-and-disappointment-in-love-sad-man-sitting-element-of-the-picture-is-decorated-by-nasa-free-photo.jpg" alt="imagen" />
       </div>
 
-      <div><p>className="text"> *Inserta un texto*</p></div>
+      <div><p>className="text" lorem ipsum Lalalalala Awawa</p></div>
 
     </div>
   )
