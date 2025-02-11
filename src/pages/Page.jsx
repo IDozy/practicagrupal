6966d3d038baf041194ea2e4e71ce5fc1f450529
Swacca <jairquispediaz@gamil.com>
2025-02-11 09:52:43 -0500

    catalogo de imagenes 2

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -8,9 +8,13 @@ const Page = () => {
         <HeaderComponent/>
       </div>
 
+      <div style={{textAlign: 'center'}}>
+        <h1>Nuestros productos</h1>
+      </div>
+
       <div style={{ display: "flex", gap: "20px", justifyContent: 'center'}}>
         <img src="/p1.PNG" alt="Imagen 1" style={{ width: "190px", height: "270px" }} />
-        <img src="/p2.PNG" alt="Imagen 2" style={{ width: "190px", height: "270px" }} />
+        <img src="/p2.PNG" alt="Imagen 2" style={{ width: "190px", height: "270px" }} /> 
         <img src="/p3.PNG" alt="Imagen 3" style={{ width: "190px", height: "270px" }} />
         <img src="/p4.PNG" alt="Imagen 4" style={{ width: "190px", height: "270px" }} />
         <img src="/p5.PNG" alt="Imagen 5" style={{ width: "190px", height: "270px" }} />
