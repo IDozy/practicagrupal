2e461714dbb062e6a029fabaf0ce3ab1663f9101
Dozy <ahuatayc18_2@unc.edu.pe>
2025-02-11 08:28:54 -0500

    Modificacion appjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import Aos from "aos";
 import "./App.css";
-import AlexComponent from "./components/AlexComponent";
+
 import Imagenescomponent from "./components/Imagenescomponent";
 import HeaderComponent from "./components/Swswaca trabajo/HeaderComponent";
 import "aos/dist/aos.css"
@@ -11,17 +11,9 @@ function App() {
   return (
     <div className="App">
       <HeaderComponent/>
-      <section id="inicio">
-        <h1>Bienvenido a la sección de Inicio</h1>
-      </section>
-      <section id="producto">
-        <h1>Nuestros Productos</h1>
-      </section>
-      <section id="contacto">
-        <h1>Contáctanos</h1>
-      </section>
-      <AlexComponent /> 
+       
       <Imagenescomponent />
+      
       
     </div>
   );
