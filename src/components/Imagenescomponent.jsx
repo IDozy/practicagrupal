e885037ec0d3dbbbabfd62c39f5db07f9a288a59
fernanCG15 <celisgelaciofernandomanuel>
2025-02-11 09:11:21 -0500

    MSS

--- src/components/Imagenescomponent.jsx
+++ src/components/Imagenescomponent.jsx
@@ -3,7 +3,7 @@ import './Imagenescomoponent.css'
 const Imagenescomponent = () => {
   return (
     <div className='container' style={{backgroundcolor:"red"}}> 
-      <div>
+      <div className='Imagen' style={{marginRight:"50px"}}>
         <img src="https://static.vecteezy.com/system/resources/thumbnails/009/273/280/small/concept-of-loneliness-and-disappointment-in-love-sad-man-sitting-element-of-the-picture-is-decorated-by-nasa-free-photo.jpg" alt="imagen" />
       </div>
 
