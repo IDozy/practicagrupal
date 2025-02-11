92316b67dddd0d0a77a9e7cb092230d3dad1aa3d
fernanCG15 <celisgelaciofernandomanuel>
2025-02-11 09:14:30 -0500

    JFS

--- src/components/Imagenescomponent.jsx
+++ src/components/Imagenescomponent.jsx
@@ -7,7 +7,7 @@ const Imagenescomponent = () => {
         <img src="https://static.vecteezy.com/system/resources/thumbnails/009/273/280/small/concept-of-loneliness-and-disappointment-in-love-sad-man-sitting-element-of-the-picture-is-decorated-by-nasa-free-photo.jpg" alt="imagen" />
       </div>
 
-      <div><p>className="text" lorem ipsum Lalalalala Awawa</p></div>
+      <div><p>className="text" Desde el Horizonte se veía aquellos jinetes que galopaban hacía su destino con intenciones de saquear las aldeas cercanas de aquello valles cálidos y tranquilos.</p></div>
 
     </div>
   )
