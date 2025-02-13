e33288ec9936579b5efe30ae57d6e4e4c7e99abf
fernanCG15 <celisgelaciofernandomanuel>
2025-02-13 08:42:47 -0500

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white text-center py-4">
+    <p>&copy; {new Date().getFullYear()} mi componente. Todos los derechos reservados.</p>
+  </footer>
+  );
+  };   
+ export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderComponent from "../components/Swswaca trabajo/HeaderComponent";
 import Imagenescomponent from "../components/Imagenescomponent";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <HeaderComponent/>
       <Imagenescomponent />
+      <Footer/>
     </div>
   );
 };
