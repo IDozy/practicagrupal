aa45b0512d8809008ca6efb3801fb9511b4c48a9
fernanCG15 <celisgelaciofernandomanuel>
2025-02-13 08:58:20 -0500

    ACTUALIZACIóN

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white text-center py-4">
     <p>&copy; {new Date().getFullYear()} mi componente. Todos los derechos reservados.</p>
+    <div className="social-icons">
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-facebook"></i>
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-twitter"></i>
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-instagram"></i>
+        </a>
+      </div>
   </footer>
+
   );
   };   
  export default Footer
